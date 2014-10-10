e78612e6e91e0f17175bca8d070b81913cf3ca97
ahvigil <ahvigil@mail.sfsu.edu>
2014-10-09 20:40:34 -0700

    add indexing demo + 12th night dataset

---
+++ Indexer.java
@@ -0,0 +1,66 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class Indexer {
+
+  public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+    private Text word = new Text();
+	private Text docid = new Text();
+
+    public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+	  FileSplit fileSplit = (FileSplit)reporter.getInputSplit();
+      String fileName = fileSplit.getPath().getName();
+		
+      String line = value.toString();
+      line = line.toLowerCase();
+      line = line.replaceAll("[^A-Za-z0-9 '-]", "");
+      line = line.replaceAll("-"," ");
+      StringTokenizer tokenizer = new StringTokenizer(line);
+	  docid.set(fileName);
+      while (tokenizer.hasMoreTokens()) {
+        word.set(tokenizer.nextToken());
+        output.collect(word, docid);
+      }
+    }
+  }
+
+  public static class Reduce extends MapReduceBase implements Reducer<Text, Text, Text, ArrayWriteable> {
+    public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, ArrayWriteable> output, Reporter reporter) throws IOException {
+      int sum = 0;
+	  ArrayWritable postingList = new ArrayWritable(Text.class);
+	  ArrayList<Text> docs = new ArrayList<>();
+      while (values.hasNext()) {
+        docs.add(values.next());
+      }
+	  postingList.set(docs.toArray(new Text[docs.size]));
+      output.collect(key, postingList);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    JobConf conf = new JobConf(WordCount.class);
+    conf.setJobName("indexer");
+
+    conf.setOutputKeyClass(Text.class);
+    conf.setOutputValueClass(ArrayWriteable.class);
+	conf.setMapOutputValueClass(Text.class);
+
+    conf.setMapperClass(Map.class);
+    conf.setCombinerClass(Reduce.class);
+    conf.setReducerClass(Reduce.class);
+
+    conf.setInputFormat(TextInputFormat.class);
+    conf.setOutputFormat(TextOutputFormat.class);
+
+    FileInputFormat.setInputPaths(conf, new Path("/user/cloudera/index/documents"));
+    FileOutputFormat.setOutputPath(conf, new Path("/user/cloudera/index/output"));
+
+    JobClient.runJob(conf);
+  }
+}
---
+++ index.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+javac -cp /usr/lib/hadoop/*:/usr/lib/hadoop/client-0.20/* Indexer.java
+jar -cvf index.jar Indexer.class
+
+hdfs dfs -rm -f -r /user/cloudera/index
+
+hdfs dfs -mkdir /user/cloudera/index/documents
+
+let docid=0
+for f in 12th_night/*
+do
+	echo "$docid $f"
+	let docid+=1
+	
+	$(hdfs dfs -put $f /user/cloudera/index/documents/$docid)
+done
+
+hadoop jar index.jar Indexer
+
+hdfs dfs -cat /user/cloudera/index/output/part-00000
